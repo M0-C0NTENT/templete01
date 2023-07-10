7346e4d9c06541dc6695e450dc17641ca074a6d8
Ali Mohamed <mohamed37222721@gmail.com>
2023-07-10 16:25:20 +0200

    fixing scroll bug

--- script.js
+++
@@ -1,5 +0,0 @@
-// Optional: If you want to prevent horizontal overscrolling as well, use the code below.
-// This prevents scrolling on the x-axis.
-document.body.addEventListener('touchmove', function (event) {
-  event.preventDefault();
-}, { passive: false });
